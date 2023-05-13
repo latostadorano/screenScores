c950e8ff12511053ec2af07f6c8131b20e7753cd
Toust <latostadorano@gmail.com>
2023-05-12 18:15:19 -0600

    Conectivity issues

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
+var cors = require('cors'); // Import the cors middleware
 var app = express();
 var server = app.listen(PORT);
 
 app.use(express.static('public'));
+app.use(cors()); // Enable cors for all routes
 console.log("My socket server is running");
 
 var socket = require('socket.io');
@@ -22,6 +24,4 @@ function newConnection(socket){
         //io.sockets.emit('mouse', data);
         console.log(data);
     }
-
-
 }
